c86723482f20174d4b9e9c82e4c7531d43743f9f
Wahaj <wahajasif488@gmail.com>
2026-04-03 19:40:07 +0500

    Remove extra commented code from all the files.

--- vectorstore/faiss_store.py
+++ vectorstore/faiss_store.py
@@ -1,39 +1,3 @@
-# import faiss
-# import numpy as np
-
-
-# class FAISSVectorStore:
-#     def __init__(self, dimension):
-#         self.index = faiss.IndexFlatL2(dimension)
-#         self.texts = []
-#         self.metadata = []
-
-#     def add_embeddings(self, embeddings, texts, metadatas):
-#         self.index.add(np.array(embeddings).astype("float32"))
-#         self.texts.extend(texts)
-#         self.metadata.extend(metadatas)
-
-#     def search(self, query_embedding, k=5, threshold=1.5):
-#         distances, indices = self.index.search(
-#             np.array([query_embedding]).astype("float32"), k
-#         )
-
-#         results = []
-#         for i, idx in enumerate(indices[0]):
-#             if idx == -1:
-#                 continue
-
-#             # ✅ FILTER LOW QUALITY MATCHES
-#             if distances[0][i] > threshold:
-#                 continue
-
-#             results.append({
-#                 "text": self.texts[idx],
-#                 "metadata": self.metadata[idx]
-#             })
-
-#         return results
-
 
 import faiss
 import numpy as np
